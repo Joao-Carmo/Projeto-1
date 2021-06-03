fa1605e6b04fd6ede2fe7b69b6985def9c7a4678
Maria Eduarda Couto <mariaeduardacoutt@gmail.com>
2021-06-03 13:40:59 +0100

    imagem_quiz

---
+++ Alvid-19/JS/models/quizModel.js
@@ -0,0 +1,10 @@
+export default class quizModel {
+    constructor(id, idUser, name, image, description, questions) {
+        this.id = id
+        this.idUser = idUser
+        this.name = name
+        this.image = image
+        this.description = description
+        this.questions = questions
+    }
+}
