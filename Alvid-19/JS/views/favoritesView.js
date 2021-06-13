af829974598c7cbaa712379d68f469abd8a03b0d
João Carmo <40190116@esmad.ipp.pt>
2021-06-13 09:41:22 +0100

    commit

--- Alvid-19/JS/views/favoritesView.js
+++ Alvid-19/JS/views/favoritesView.js
@@ -4,11 +4,13 @@ export default class favoritesView {
     constructor() {
         this.userController = new userController();
 
+        this.thumbnailQuizzes = document.querySelector('#thumbnailQuizzes')
+        this.quizzesImages();
+
         this.profileIcon = document.querySelector('#profileIcon');
         this.updateStatusUI();
 
-        this.thumbnailQuizzes= document.querySelector('#thumbnailQuizzes')
-        this.thumbnailQuizzes()
+        
     }
 
     updateStatusUI() {
@@ -25,21 +27,21 @@ export default class favoritesView {
             this.profileIcon.innerHTML = `
                 <a class="nav-link" href="HTML/userAuthentication.html">
                     <img src="Images/perfil.png" width="84px" alt="Ícone Perfil" id="iconPerfil" id="iconPerfil" style="filter: drop-shadow(0px 9px 3px #0a9cb6);border-radius: 50px">
-                </a>`
-            
+                </a>` 
         }
     }
 
-    thumbnailQuizzes(){
-        this.thumbnailQuizzes.innerHTML= `
+    quizzesImages() {
+
+      this.thumbnailQuizzes.innerHTML += `
         <div class="mt-5 row">
-        <div class="col-lg-4 col-6">
-          <div class="hero" >
-            <a href="../Alvid-19/HTML/quizzes.html">
-              <img src="Images/Como ser Herói!.png" id="quizCarrossel" class="mt-5 col-sm-7 ml-4 mb-5" alt="Responsive image">
-            </a>
+          <div class="col-lg-4 col-6">
+            <div class="hero">
+              <a href="HTML/quizzes.html">
+                <img src="Images${}" id="quizCarrossel" class="mt-5 col-sm-7 ml-4 mb-5" alt="Responsive image">
+              </a>
+            </div>
           </div>
-        </div>
-      </div>`
+        </div>`
     }
 }
