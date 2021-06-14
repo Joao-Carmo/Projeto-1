c5c58606107b8258007e3331065a5329b5c2a74d
Viviana <400200339@esmad.ipp.pt>
2021-06-14 02:45:18 +0100

    +adicionada determinada responsividade de funções conforme o utilizador logado(ou não logado)

--- Alvid-19/JS/views/favoritesView.js
+++ Alvid-19/JS/views/favoritesView.js
@@ -6,7 +6,12 @@ export default class favoritesView {
 
         this.helpButton = document.querySelector('#helpButton')
         this.btnAdminManagement();
-        
+
+        this.hangmanGame = document.querySelector('#hangmanGame')
+        this.memoryGame = document.querySelector('#memoryGame')
+        this.iconRanking = document.querySelector('#iconRanking')
+        this.updatePermissions();
+
         this.thumbnailQuizzes = document.querySelector('#thumbnailQuizzes')
         this.quizzesImages();
 
@@ -31,6 +36,38 @@ export default class favoritesView {
                     <img src="Images/perfil.png" width="84px" alt="Ícone Perfil" id="iconPerfil" id="iconPerfil" style="filter: drop-shadow(0px 9px 3px #0a9cb6);border-radius: 50px">
                 </a>` 
         }
+    }
+
+    /**
+     * Função que verifica se, conforme o tipo de utilizador logado(se logado), poderá ter permissão para jogos/quizzes/ranking. 
+     */
+    updatePermissions() {
+      if (!this.userController.isLogged() || this.userController.isBlocked()) {
+        this.memoryGame.innerHTML = `
+          <a href="HTML/userAuthentication.html">
+            <img src="Images/Jogo da Memoria.png" alt="Responsive image" class="img-fluid" id="gameCarousel">
+          </a>`
+
+        this.hangmanGame.innerHTML = `
+          <a href="HTML/userAuthentication.html">
+            <img src="Images/Jogo da f rca.png" alt="Responsive image" class="img-fluid" id="gameCarousel">
+          </a>`
+
+        this.iconRanking.parentNode.href = 'HTML/userAuthentication.html'
+
+      } else {
+        this.memoryGame.innerHTML = `
+          <a href="HTML/jogoDaMemoria.html">
+            <img src="Images/Jogo da Memoria.png" alt="Responsive image" class="img-fluid" id="gameCarousel">
+          </a>`
+
+        this.hangmanGame.innerHTML = `
+          <a href="HTML/jogoDaForca.html">
+            <img src="Images/Jogo da f rca.png" alt="Responsive image" class="img-fluid" id="gameCarousel">
+          </a>`
+
+        this.iconRanking.parentNode.href = 'HTML/ranking.html'
+      }
     }
 
     quizzesImages() {
